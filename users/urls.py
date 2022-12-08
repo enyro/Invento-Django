54276f74c0ea29297efcdb5e1056a8393b5de349
enyro <enyrofernando@outlook.com>
2022-12-08 22:58:40 +0530

    User module updated

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [ 
-    path('create-user',views.create,name='create-user'),
+urlpatterns = [  
     path('signin',views.signin,name='signin'),
     path('signout',views.signout,name='signout'),
     path('users',views.users,name='users'),
+
+    path('api/v1/signin', views.signinApi, name="sigin-api"),
+    path('api/v1/users', views.getUsers, name="users-api"),
+    path('api/v1/create-user', views.create, name='create-user-api'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,59 +1,81 @@
 from django.shortcuts import render,redirect
-from users.models import role, user_role
-from django.contrib import messages
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from users.models import role, user_role 
+from django.contrib.auth.models import User 
 from .forms import RegisterForm
 from django.contrib.auth import authenticate, login, logout
+from django.http import JsonResponse
 
-# Create your views here.
-def create(request):
-    if request.method == 'GET':
-        roles = role.objects.order_by('id').all()
-        form  = RegisterForm() 
-        return render(request, 'create-user.html', {'form': form, 'roles':roles})
-    if request.method == 'POST':
-        form  = RegisterForm(request.POST)
-       
-        if form.is_valid():
-            role_id = request.POST['role']
-            role_data = role.objects.get(id=role_id)
-            form.save()
-            username = form.cleaned_data.get('username')
-            user_data = User.objects.get(username=username)
-            add_user_role = user_role(
-                user = user_data,
-                role = role_data
-            )
-            add_user_role.save()
-            messages.success(request, 'Account was created for ' + username)
-            return redirect('users')
-        else:
-            print('Form is not valid')
-            messages.error(request, 'Error Processing Your Request')
-            roles = role.objects.order_by('id').all() 
-            return render(request, 'create-user.html', {'form': form, 'roles':roles})
-    return render(request, 'create-user.html', {})
 def signin(request):
-    logout(request)
-    if request.method == "GET": 
-        return render(request,'signin.html',{})
-    if request.method == "POST": 
-        username = request.POST['username']
-        password = request.POST['password']
-
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return redirect('home')
-         
-    return render(request,'signin.html',{})
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        return render(request, 'signin.html', {}) 
 
 def signout(request):
-    logout(request) 
+    logout(request)
     return redirect('signin')
 
-def users(request):
-    users = User.objects.order_by('id').all()
-    return render(request,'users.html',{'nbar':'users','users':users})
+def signinApi(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return JsonResponse({'id': 200, 'data':"Signed in successfully!"})
+
+        return JsonResponse({'id': 400, 'data': "Entered user is not active!"})
+
+    return JsonResponse({'id': 400, 'data': "Invalid username or password!"})
+
+def users(request): 
+    if request.user.is_authenticated and request.user.user_role.role.role == 'Admin':
+        roles = role.objects.order_by('id').all()
+        form = RegisterForm()
+        return render(request, 'users.html', {'nbar': 'users', 'form': form, 'roles': roles})
+    else:
+        return redirect('home')
+
+def getUsers(request):
+    if request.user.is_authenticated and request.user.user_role.role.role == 'Admin':
+        roleId = request.GET['role']
+        if roleId != '0':
+            roleFilter = role.objects.get(id=roleId)
+            users = User.objects.order_by('id').all().values('id', 'username','email','first_name','last_name','user_role__role__role').filter(user_role__role=roleFilter)
+        else:
+            users = User.objects.order_by('id').all().values('id', 'username','email','first_name','last_name','user_role__role__role')
+
+        return JsonResponse({'id': 200, 'data': list(users)})
+
+def create(request): 
+    if request.user.is_authenticated and request.user.user_role.role.role == 'Admin':
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        roleId = request.POST['role']
+        email = request.POST['email']
+        password = request.POST['password'] 
+
+        usernameExist = User.objects.filter(username=username).count()
+        if usernameExist == 0 :
+            create_user = User(
+                email=email,
+                first_name=fname,
+                last_name=lname,
+                username=username,
+                password=password 
+            )
+            create_user.save()
+
+            if create_user.id is not None:
+                role_data = role.objects.get(id=roleId)
+                add_user_role = user_role(
+                    user=create_user,
+                    role = role_data
+                )
+                add_user_role.save()
+                return JsonResponse({'id': 200, 'data': "User created successfully!!"})
+            else:
+                return JsonResponse({'id': 400, 'data': "Failed to create!!"})
+        else:
+            return JsonResponse({'id': 400, 'data': "Username already exists!!"})
